d49fcdbce91d445b7b93e9fc54949333c1d06395
andrey <Andrey.Averyanov@infobip.com>
2016-04-07 11:23:08 +0300

    add examples for rabbitmq via web-sockets, but those doesn't work

---
+++ examples/rabbitMQ/inBrowser/server.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = 1337;
+var express = require("express");
+
+var app = express();
+
+app.use(express.static(__dirname + "/client/public")); //use static files in ROOT/public folder
+
+app.get("/", function(request, response){ //root dir
+    response.send("Hello!!");
+});
+
+app.listen(port, host, function() {
+    console.log('Server listen on port: ', port);
+});
