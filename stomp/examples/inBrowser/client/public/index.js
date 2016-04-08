2dfdb0e891eb2a86ee3f2f726b41dfc192d395be
andrey <Andrey.Averyanov@infobip.com>
2016-04-08 12:32:09 +0300

    change message ro JSON in example

--- stomp/examples/inBrowser/client/public/index.js
+++ stomp/examples/inBrowser/client/public/index.js
@@ -6398,15 +6398,21 @@ const SockJS = require('sockjs-client');
 
 const ws = new SockJS('http://127.0.0.1:15674/stomp');
 let client = StompJS.over(ws);
+client.debug = false;
 
 client.connect('guest', 'guest',
     () => {
         console.log('connect to stomp');
 
-        client.send("/queue/test", {priority: 9}, "Hello, STOMP");
+        const payload = {
+            message: 'Hello, STOMP'
+        };
 
-        client.subscribe("/queue/test", message => {
-            console.log('Subscribe: ', message.body);
+        client.send("/queue/test", {priority: 9}, JSON.stringify(payload));
+
+        client.subscribe("/queue/test", data => {
+            const payload = JSON.parse(data.body);
+            console.log('Subscribe: ', payload);
         });
     },
     error => {
--- stomp/examples/inBrowser/client/src/index.js
+++ stomp/examples/inBrowser/client/src/index.js
@@ -2,16 +2,22 @@ const StompJS = require('stompjs');
 const SockJS = require('sockjs-client');
 
 const ws = new SockJS('http://127.0.0.1:15674/stomp');
-let client = StompJS.over(ws);
+const client = StompJS.over(ws);
+client.debug = false;
 
 client.connect('guest', 'guest',
     () => {
         console.log('connect to stomp');
 
-        client.send("/queue/test", {priority: 9}, "Hello, STOMP");
+        const payload = {
+            message: 'Hello, STOMP'
+        };
 
-        client.subscribe("/queue/test", message => {
-            console.log('Subscribe: ', message.body);
+        client.send("/queue/test", {}, JSON.stringify(payload));
+
+        client.subscribe("/queue/test", data => {
+            const payload = JSON.parse(data.body);
+            console.log('Subscribe: ', payload);
         });
     },
     error => {
